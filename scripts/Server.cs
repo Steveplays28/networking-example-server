3a2e12c338f71b6c1b7b9bae45476977089be64b
Darion Spaargaren <djspaargaren@outlook.com>
2022-01-18 17:59:38 +0100

    Rollback to latest working commit

--- scripts/Server.cs
+++ scripts/Server.cs
@@ -12,30 +12,24 @@ public class Server : Node
 
 	public struct UdpState
 	{
-		// Local (server) endpoint
 		public IPEndPoint endPoint;
 		public UdpClient udpClient;
+		// Not implemented
+		// TODO: implement packetCount
 		public int packetCount;
-		// TODO: implement packet ids and checks
 
-		// Stored and connected clients (clientId, udpClient)
-		public Dictionary<int, UdpClient> savedClients;
+		// Connected clients
 		public Dictionary<int, UdpClient> connectedClients;
-		// TODO: implement client connections
-		// TODO: implement afk kick
+		// Saved clients
+		public Dictionary<int, UdpClient> savedClients;
 	}
 	public static UdpState udpState = new UdpState();
-
-	public static Dictionary<int, Action<int, Packet>> packetFunctions = new Dictionary<int, Action<int, Packet>>()
-	{
-		{ 0, OnConnected }
-	};
 
 	public override void _Ready()
 	{
 		CreateUdpClient(mainIp, mainPort);
 
-		using (Packet packet = new Packet(0, 0, -1))
+		using (Packet packet = new Packet(0, 0, 0))
 		{
 			packet.WriteData("hi there :)");
 
@@ -46,39 +40,12 @@ public class Server : Node
 	public static void CreateUdpClient(string ip, string port)
 	{
 		udpState.endPoint = new IPEndPoint(IPAddress.Parse(ip), port.ToInt());
-		udpState.udpClient = new UdpClient(udpState.endPoint);
+		udpState.udpClient = new UdpClient();
 		udpState.packetCount = 0;
-
-		GD.Print(udpState.endPoint.ToString());
 
 		udpState.connectedClients = new Dictionary<int, UdpClient>();
 	}
 
-	public static bool IsClientSaved(int clientId)
-	{
-		return udpState.savedClients.ContainsKey(clientId);
-	}
-	public static bool IsClientSaved(IPEndPoint clientEndPoint)
-	{
-		bool isClientSaved = false;
-
-		foreach (UdpClient udpClient in udpState.savedClients.Values)
-		{
-			if (udpClient.Client.LocalEndPoint == clientEndPoint)
-			{
-				isClientSaved = true;
-			}
-		}
-
-		return isClientSaved;
-	}
-
-	public static bool IsClientConnected(int clientId)
-	{
-		return udpState.connectedClients.ContainsKey(clientId);
-	}
-
-	#region Sending packets
 	public static void SendPacket(Packet packet)
 	{
 		// Write packet header
@@ -97,6 +64,8 @@ public class Server : Node
 		UdpClient udpClient = (UdpClient)asyncResult.AsyncState;
 
 		GD.Print($"Amount of bytes sent: {udpClient.EndSend(asyncResult)}");
+		GD.Print($"Local endpoint: {udpState.udpClient.Client.LocalEndPoint}");
+		GD.Print($"Remote endpoint: {udpState.endPoint}");
 	}
 
 	public static void SendPacketTo(int clientId, Packet packet)
@@ -117,55 +86,4 @@ public class Server : Node
 
 		GD.Print($"Amount of bytes sent: {udpClient.EndSend(asyncResult)}");
 	}
-	#endregion
-
-	#region Receiving packets
-	private static void ReceiveCallback(IAsyncResult asyncResult)
-	{
-		// Called when a packet is received
-		byte[] receiveBytes = udpState.udpClient.EndReceive(asyncResult, ref udpState.endPoint);
-		udpState.packetCount += 1;
-
-		// Continue listening for packets
-		udpState.udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), udpState.udpClient);
-
-		// Handle received packet
-		using (Packet packet = new Packet(receiveBytes))
-		{
-			// Debug, lol
-			GD.Print(string.Join(",", receiveBytes));
-
-			// Check if client exists in the savedClients dictionary
-			// if (!IsClientSaved(senderEndPoint))
-			// {
-			// 	// If not, add the client to the savedClients dictionary
-			// 	int clientId = udpState.savedClients.Count;
-			// 	UdpClient udpClient = new UdpClient(senderEndPoint);
-
-			// 	udpState.savedClients.Add(clientId, udpClient);
-			// }
-
-			// TODO: check if client exists in connectedClients dictionary
-
-			// Invoke the packet's connected function
-			packetFunctions[packet.connectedFunction].Invoke(packet.clientId, packet);
-
-			// TODO: check for dropped packets using packetNumber, and let the server resend a list of packets if needed
-			// TODO: use checksum to check for data corruption/data loss in the packet
-		}
-	}
-	#endregion
-
-	#region Packet callbacks
-	// Packet callback functions must be static, else they cannot be stored in the packetFunctions dictionary
-	private static void OnConnected(int clientId, Packet packet)
-	{
-		// using (Packet newPacket = new Packet(0, 0, clientId))
-		// {
-		// 	newPacket.WriteData("hi there :)");
-
-		// 	SendPacket(newPacket);
-		// }
-	}
-	#endregion
 }
